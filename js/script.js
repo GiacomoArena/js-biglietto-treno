d3555be24725a688fc7ee7bfbb6fa699b3c772dd
GiacomoArena <giacomoarena28@gmail.com>
2023-03-20 17:34:27 +0100

    last commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 
+const data = new Date();
+const giorno = data.getDate() + "/";
+const mese = data.getMonth() + 1 + "/";
+const anno = data.getFullYear();
 
-const km = prompt("Ciao, quanti KM hai intenzione di percorrere?");
+const nome = prompt('Come ti chiami? nome cognome:')
+
+const km = prompt("quanti KM hai intenzione di percorrere?");
 
 console.log('km da percorrere', km);
 
@@ -14,15 +20,22 @@ let prezzo = 0.21 * km;
 
 if(age < 18){
   prezzo -= (prezzo*20)/100;
-  document.getElementById('prezzo_biglietto').innerHTML = "Prezzo Under18: " + prezzo.toFixed(2) + "&euro;";
+  document.getElementById('prezzo_biglietto').innerHTML = "Biglietto Under18: " + prezzo.toFixed(2) + "&euro;";
 }
 else if (age >= 65) {
   prezzo -= (prezzo*40)/100;
-  document.getElementById('prezzo_biglietto').innerHTML = "Prezzo Over65: " + prezzo.toFixed(2) + "&euro;";
+  document.getElementById('prezzo_biglietto').innerHTML = "Biglietto Over65: " + prezzo.toFixed(2) + "&euro;";
 }
 else {
   document.getElementById('prezzo_biglietto').innerHTML =
-  "Prezzo Standard: " + prezzo.toFixed(2) + "&euro;";
+  "Biglietto Standard: " + prezzo.toFixed(2) + "&euro;";
 }
 
 console.log('il prezzo é ', prezzo.toFixed(2));
+
+document.getElementById('nomecognome').innerHTML = nome;
+
+
+document.getElementById('anni').innerHTML = age + " anni";
+
+document.getElementById('data').innerHTML = giorno + mese + anno;
